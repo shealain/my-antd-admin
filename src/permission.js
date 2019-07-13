2b3cc702bd3252148e840f3b6ffca86c2a9cacb7
kaiyu5609 <1047935422@qq.com>
2019-07-13 11:20:27 +0800

    add nprogress
    添加路由切换进度条
    并且设置页面的标题

--- src/permission.js
+++ src/permission.js
@@ -1,11 +1,21 @@
 import router from './router'
 import store from './store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 import { getToken } from '@/utils/auth'
+import Message from 'ant-design-vue/es/message'
+import getPageTitle from '@/utils/get-page-title'
 
+NProgress.configure({ showSpinner: false })
 
 const whiteList = ['/login']// 不需重定向的白名单
 
 router.beforeEach(async (to, from, next) => {
+  // 开始进度条
+  NProgress.start()
+
+  // 设置页面标题
+  document.title = getPageTitle(to.meta.title)
 
   window.console.log('@/permission/to:', to.path)
 
@@ -18,6 +28,7 @@ router.beforeEach(async (to, from, next) => {
     if (to.path === '/login') {
       // 如果已登录，则重定向到主页
       next({ path: '/' })
+      NProgress.done()
     } else {
       // 确定用户是否通过getInfo获取了他的权限角色
       const hasRoles = store.getters.roles && store.getters.roles.length > 0
@@ -44,7 +55,9 @@ router.beforeEach(async (to, from, next) => {
         } catch (err) {
           // 移除 token，跳到登录页面去重新登录
           await store.dispatch('user/resetToken')
+          Message.error(err || 'Has Error')
           next(`/login?redirect=${to.path}`)
+          NProgress.done()
         }
       }
     }
@@ -56,10 +69,12 @@ router.beforeEach(async (to, from, next) => {
     } else {
       // 其他无权访问的页面将重定向到登录页面。
       next(`/login?redirect=${to.path}`)
+      NProgress.done()
     }
   }
 })
 
 router.afterEach(() => {
-
+  // 完成进度条
+  NProgress.done()
 })
